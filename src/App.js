d84c9d9903f0854982023b64a89d2ea8dd06531a
Walter Ronny Ibanez Saucedo <walteribanez555@gmail.com>
2022-12-17 17:40:49 -0400

    create private route and custom hook for protection the profile and add the logout functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import Home from "./pages/Home";
+import PrivateRoute from "./components/PrivateRoute"
 import Profile from "./pages/Profile";
 import Offers from "./pages/Offers";
 import SignIn from "./pages/SignIn";
@@ -21,7 +22,9 @@ function App() {
 
           <Routes>
             <Route path="/" element={<Home/>}/>
-            <Route path="/profile" element={<Profile/>}/>
+            <Route path="/profile" element={<PrivateRoute/>}>
+              <Route path="/profile" element={<Profile/>}/>
+            </Route>
             <Route path="/sign-in" element={<SignIn/>}/>
             <Route path="/sign-up" element={<SignUp/>}/>
             <Route path="/forgot-password" element={<ForgotPassword/>}/>
