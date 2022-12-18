a5f55746bc2f6a4d1e87fde60610921e96b71fb3
Walter Ronny Ibanez Saucedo <walteribanez555@gmail.com>
2022-12-18 16:05:37 -0400

    complete the listingItem component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -69,7 +69,7 @@ export default function Profile() {
         let listings = [];
         querySnap.forEach((doc) => {
           return listings.push({ 
-            i: doc.id,
+            id: doc.id,
             data: doc.data(),
           });
         });
@@ -130,11 +130,15 @@ export default function Profile() {
       <div className="max-w-6xl px-3 mt-6  mx-auto">
             {!loading && listings.length > 0 && (
               <>
-                <h2 className="text-2xl text-center font-semibold">My Listings</h2>
+                <h2 className="text-2xl text-center font-semibold mb-6 ">My Listings</h2>
 
-                <ul>
+                <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl-grid-cols-5 mt-6 mb-6">
                   {listings.map((listing)=> (
-                    <ListingItem key={listing.id} id={listing.id}  listing={listing.data} />
+                    <ListingItem
+                     key={listing.id}
+                     id={listing.id}
+                     listing={listing.data} 
+                     />
                   ))}
                 </ul>
 
