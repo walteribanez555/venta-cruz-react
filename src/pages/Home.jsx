d7e9cce32284a96342da074313034fa6e900d6a5
Walter Ronny Ibanez Saucedo <walteribanez555@gmail.com>
2022-12-19 22:16:07 -0400

    create the places for rent and sales pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -163,7 +163,7 @@ export default function Home() {
             <h2 className="px-3 text-2xl mt-6 font-semibold">
               places for sales
             </h2>
-            <Link to="/sales">
+            <Link to="/category/sale">
               <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">
                 Show more sales
               </p>
